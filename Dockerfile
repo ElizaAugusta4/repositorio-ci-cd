bddceb5e43f841454550b55ac698b334849830ae
ElizaAugusta4 <eliza.19.bz@outlook.com>
2023-10-06 14:13:53 -0300

    C

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+# Dockerfile
+
+FROM python:3.8
+
+WORKDIR /app
+
+COPY requirements.txt .
+
+RUN pip install -r requirements.txt
+
+COPY src/ src/
+
+CMD ["python", "-m", "src.main"]
--- src/main.py
+++ src/main.py
@@ -1,7 +1,17 @@
 # main.py
-
+import unittest
 def soma(a, b):
     return a + b
 
 def subtracao(a, b):
     return a - b
+
+
+class TestFuncoes(unittest.TestCase):
+    def test_soma(self):
+        self.assertEqual(soma(2, 3), 5)
+
+    def test_subtracao(self):
+        self.assertEqual(subtracao(5, 3), 2)
+        
+  
